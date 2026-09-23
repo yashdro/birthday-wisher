e04c440a7f7110eca64e0334049b1c8ef04dc771
yashdro <yashdrolia02@gmail.com>
2026-09-23 21:57:18 +1000

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ my_email = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 
-
-birthday_yay = dt.datetime.now()
-
-today_month = birthday_yay.month
-today_day = birthday_yay.day
-
+now = dt.datetime.now()
+today_month = now.month
+today_day = now.day
 today = (today_month,today_day)
+day_of_week = now.weekday()
+
 
 print(today)
 
@@ -42,6 +41,30 @@ if today in new_dict:
         connection.starttls()
         connection.login(user=my_email, password= MY_PASSWORD)
         connection.sendmail(from_addr=my_email, to_addrs=birthday_person["email"], msg=f"Subject:Happy Birthday!\n\n{contents}")
+
+
+
+
+print(day_of_week)
+
+with open("quotes.txt", "r", encoding="utf-8") as quote:
+    contents = quote.readlines()
+
+if day_of_week == 0:
+    random_quote = random.choice(contents)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=MY_PASSWORD)
+        connection.sendmail(from_addr=my_email, to_addrs="yashdrolia03@gmail.com", msg=f"Subject: Quote of the day\n\n{random_quote}".encode("utf-8"))
+        print("Quote Sent")
+
+else:
+    print("today is not monday no quote sent")
+
+
+
+
+
 
 
 
